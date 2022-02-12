a0cb8c865914920bd845cc98d116a0f6454ff99d
Ramiro Tepehua <rtface@gmail.com>
2022-02-12 06:42:18 -0600

    modifica el router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/oic',
+    pathMatch: 'full'
+  },
+  {
+    path: 'oic',
+    loadChildren: () => import('./oic/oic.module').then(m => m.OicModule)
+  },
+  {
     path: '404',
     loadChildren: () => import('./notfound/notfound.module').then(m => m.NotfoundModule)
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'oic',
-    loadChildren: () => import('./oic/oic.module').then(m => m.OicModule)
   },
   {
     path: '**',
