322e033d64c1e4bb1776e951e9693ddaebdabbf4
Ramiro Tepehua <rtcface@gmail.com>
2022-02-22 10:09:25 -0600

    ajustes al login a los textos a footer

--- src/app/shared/pages/nav-menu/nav-menu.component.ts
+++ src/app/shared/pages/nav-menu/nav-menu.component.ts
@@ -19,17 +19,17 @@ export class NavMenuComponent implements OnInit {
           routerLink: '/oic/home'
       },
       {
-          label: 'Datos Del OIC',
+          label: 'Datos del OIC',
           icon: 'pi pi-fw pi-search',
           routerLink: '/oic/datos-generales'
       },
       {
-          label: 'Plan De Trabajo',
+          label: 'Plan de Trabajo',
           icon: 'pi pi-fw pi-list',
           routerLink: '/oic/plan-trabajo'
       },
       {
-          label: 'Estadisticas De Procesos',
+          label: 'Estadisticas de Procesos',
           icon: 'pi pi-fw pi-chart-bar',
           routerLink: '/oic/kpis'
       },
