a613129b38b06486e633a353fcf576d45dd3493c
Ramiro Tepehua <rtcface@gmail.com>
2023-09-05 14:57:24 -0600

    se cambia el titulo retirando la palabra [de los Entes]

--- src/assets/constantes/constantes.ts
+++ src/assets/constantes/constantes.ts
@@ -3,10 +3,10 @@ export class Constantes {
     public static readonly uri_icon_oic = 'assets/img/oic-icon.png';
     public static readonly uri_icon_plt = 'assets/img/plt-icon.png';
     public static readonly title_card_oic = `Portal de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala`;
-    public static readonly title_card_plt = `Portal de Ética Pública de los Entes del Estado de Tlaxcala`;
-    public static readonly footer_plt = `Portal de Ética Pública de los Entes del Estado de Tlaxcala`;
+    public static readonly title_card_plt = `Portal de Ética Pública del Estado de Tlaxcala`;
+    public static readonly footer_plt = `Portal de Ética Pública del Estado de Tlaxcala`;
     public static readonly footer_oic = `Portal de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala`;
-    public static readonly header_plt = `Portal de Ética Pública de los Entes del Estado de Tlaxcala`;
+    public static readonly header_plt = `Portal de Ética Pública del Estado de Tlaxcala`;
     public static readonly header_oic = `Portal de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala `;
     public static readonly header_oic_p1 = `Portal de Órganos Internos de Control`;
     public static readonly header_oic_p2 = `de los Entes Públicos del Estado de Tlaxcala`;
