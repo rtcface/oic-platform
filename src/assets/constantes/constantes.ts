dc586990d5d2f8112603f2f340adfd01894154c2
Ramiro Tepehua <rtcface@gmail.com>
2025-01-23 13:22:29 -0600

    corrección a los titulos de los micrositios

--- src/assets/constantes/constantes.ts
+++ src/assets/constantes/constantes.ts
@@ -1,16 +1,13 @@
 // create class constantes
 export class Constantes {
-    public static readonly uri_icon_oic = 'assets/img/oic-icon.png';
-    public static readonly uri_icon_plt = 'assets/img/plt-icon.png';
-    public static readonly title_card_oic = `Portal de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala`;
-    public static readonly title_card_plt = `Portal de Ética Pública del Estado de Tlaxcala`;
-    public static readonly footer_plt = `Portal de Ética Pública del Estado de Tlaxcala`;
-    public static readonly footer_oic = `Portal de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala`;
-    public static readonly header_plt = `Portal de Ética Pública del Estado de Tlaxcala`;
-    public static readonly header_oic = `Portal de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala `;
-    public static readonly header_oic_p1 = `Portal de Órganos Internos de Control`;
-    public static readonly header_oic_p2 = `de los Entes Públicos del Estado de Tlaxcala`;
-
-    }
-
-    
+  public static readonly uri_icon_oic = 'assets/img/oic-icon.png';
+  public static readonly uri_icon_plt = 'assets/img/plt-icon.png';
+  public static readonly title_card_oic = `Micrositio de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala`;
+  public static readonly title_card_plt = `Micrositio de Ética Pública del Estado de Tlaxcala`;
+  public static readonly footer_plt = `Micrositio de Ética Pública del Estado de Tlaxcala`;
+  public static readonly footer_oic = `Micrositio de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala`;
+  public static readonly header_plt = `Micrositio de Ética Pública del Estado de Tlaxcala`;
+  public static readonly header_oic = `Micrositio de Órganos Internos de Control de los Entes Públicos del Estado de Tlaxcala `;
+  public static readonly header_oic_p1 = `Micrositio de Órganos Internos de Control`;
+  public static readonly header_oic_p2 = `de los Entes Públicos del Estado de Tlaxcala`;
+}
