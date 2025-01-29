cb6166018e57ca729e117d357249540f21438cb5
Ramiro Tepehua <rtcface@gmail.com>
2025-01-29 13:05:29 -0600

    Se cambian los logos del encabezado

--- src/app/protected/pages/adm-plt-rules/adm-plt-rules.component.ts
+++ src/app/protected/pages/adm-plt-rules/adm-plt-rules.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import {  history_update } from 'src/app/shared/models/history.interface';
+import { history_update } from 'src/app/shared/models/history.interface';
 import { kpiByEnteQueryInput } from '../../models/kpis.interface';
 import { ProtectedService } from '../../services/protected.service';
 import { history_init } from '../../../shared/models/history.interface';
@@ -13,83 +13,81 @@ import { MessageService } from 'primeng/api';
   providers: [MessageService],
 })
 export class AdmPltRulesComponent implements OnInit {
-  data:history_update = { } as history_update;
-  ent:kpiByEnteQueryInput = {} as kpiByEnteQueryInput;
+  data: history_update = {} as history_update;
+  ent: kpiByEnteQueryInput = {} as kpiByEnteQueryInput;
 
-  constructor(  
+  constructor(
     private readonly auServ: AuthService,
     private readonly ps: ProtectedService,
-    private readonly ms: MessageService,
-    ) { }
+    private readonly ms: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.loadRules();
   }
 
-  loadRules(){
+  loadRules() {
     this.ent.ente_publico = this.auServ.idEnteAuth;
     this.ps.getIntegrationRules(this.ent).subscribe({
-    next: (result) => {
-      if (result.data?.rules[0] !== null && result.data?.rules[0] !== undefined) {        
-        this.data = result.data?.rules[0]!;
-        //console.log("in the parent",this.data);
-        // //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>> Desde el load ",this.data);
-      } else {
-        const register:history_init={
-          ente_publico: this.ent.ente_publico
-         }        
-        this.ps.initRules(register).subscribe(
-          {
-            next:(result) => {
+      next: (result) => {
+        if (
+          result.data?.rules[0] !== null &&
+          result.data?.rules[0] !== undefined
+        ) {
+          this.data = result.data?.rules[0]!;
+          //console.log("in the parent",this.data);
+          // //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>> Desde el load ",this.data);
+        } else {
+          const register: history_init = {
+            ente_publico: this.ent.ente_publico,
+          };
+          this.ps.initRules(register).subscribe({
+            next: (result) => {
               //console.log(result);
             },
             error(err) {
-                //console.log(err);
+              //console.log(err);
             },
-            complete: () => {             
+            complete: () => {
               this.loadRules();
-            }
-          }
-        );        
-      }      
-    },  
-    error: (err) => {
-       //console.log("error", err);
-    },
-    complete: () => {
-       //console.log('compleate')
-      }
-   });
-
-  }
-
-  saveSubmit(ruleSave:history_update){  
-   
-    this.ps.updateRules(ruleSave).subscribe({
-      next: (result) => {
-
-        if(result.data !== null ){
-          this.showMessageDinamic( "success", 'Actualización', 'Las reglas de integracion se actualizaron correctament...');
-        }else{
-          this.showMessageDinamic( "error", 'Error', 'Algo sali mal ;-(');
-          //console.log(`Err ${result.errors}`);
-        }        
-      
-      }, error: (err) => {
-        this.showMessageDinamic( "error", 'Error', 'Algo sali mal ;-(');
-         //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>Error en la consulta",err);
+            },
+          });
+        }
+      },
+      error: (err) => {
+        //console.log("error", err);
       },
       complete: () => {
-        this.loadRules();        
+        //console.log('compleate')
       },
-    });      
+    });
   }
 
-
-  showMessageDinamic(severity:string ,summary: string, detail: string) {
-    this.ms.add({ severity, summary, detail}); //<-- Mensaje de error
+  saveSubmit(ruleSave: history_update) {
+    this.ps.updateRules(ruleSave).subscribe({
+      next: (result) => {
+        if (result.data !== null) {
+          this.showMessageDinamic(
+            'success',
+            'Actualización',
+            'Las reglas de integridad se actualizaron correctament...'
+          );
+        } else {
+          this.showMessageDinamic('error', 'Error', 'Algo sali mal ;-(');
+          //console.log(`Err ${result.errors}`);
+        }
+      },
+      error: (err) => {
+        this.showMessageDinamic('error', 'Error', 'Algo sali mal ;-(');
+        //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>Error en la consulta",err);
+      },
+      complete: () => {
+        this.loadRules();
+      },
+    });
   }
 
-
-
+  showMessageDinamic(severity: string, summary: string, detail: string) {
+    this.ms.add({ severity, summary, detail }); //<-- Mensaje de error
+  }
 }
